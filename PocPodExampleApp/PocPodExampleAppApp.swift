e67de7dff9fd63aa1cfbb07942118dc2a1790564
Virtual Machine <virtualmachine@MacBook-Pro-de-Virtual-2.local>
2022-10-19 11:53:57 -0300

    first commit

---
+++ PocPodExampleApp/PocPodExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocPodExampleAppApp.swift
+//  PocPodExampleApp
+//
+//  Created by Virtual Machine on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PocPodExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocPods/PocPods.h
@@ -0,0 +1,18 @@
+//
+//  PocPods.h
+//  PocPods
+//
+//  Created by Virtual Machine on 18/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PocPods.
+FOUNDATION_EXPORT double PocPodsVersionNumber;
+
+//! Project version string for PocPods.
+FOUNDATION_EXPORT const unsigned char PocPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocPods/PublicHeader.h>
+
+
